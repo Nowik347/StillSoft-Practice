3f4ed15ae95bae5ef986976eabea171e1698fdcb
Nowik347 <nowik347@mail.ru>
2023-06-20 16:37:32 +0300

    Жёска нарезал код

--- NumberToTextConverter/NumberToTextConverter.cpp
+++ NumberToTextConverter/NumberToTextConverter.cpp
@@ -3,183 +3,192 @@
 #include <Windows.h>
 #include <conio.h>
 
-using namespace std;
+const std::string g_strings[5][10]{                             // Глобальный словарь
+    { "", " один", " два", " три", " четыре", " пять", " шесть", " семь", " восемь", " девять" },
+    { "", " одиннадцать", " двенадцать", " тринадцать", " четырнадцать", " пятнадцать", " шестнадцать", " семнадцать", " восемнадцать", " девятнадцать" },
+    { "", " десять", " двадцать", " тридцать", " сорок", " пятьдесят", " шестьдесят", " семьдесят", " восемьдесят", " девяносто" },
+    { "", " сто", " двести", " триста", " четыреста", " пятьсот", " шестьсот", " семьсот", " восемьсот", " девятьсот" },
+    { " рубль", " рубля", " рублей", " тысяча", " тысячи", " тысяч", " миллион", " миллиона", " миллионов", ""} };
 
-void numToTextConvert(char numbers[], bool trigger)
+std::string writeToOutput(char trichar[], int cycles)           // Вспомогательная функция конвертации
 {
-    // Разбивающий массив
-    char trichar[3] { 0,0,0 };
-    // Счётчики
-    int count{ 0 }, cycles{ 0 };
+    int char1{ trichar[0] - '0' },                              // Временные переменные 
+        char2{ trichar[1] - '0' },                              // (символы хранятся как числа ASCII таблицы так что можно просто вычесть символ нуля(0) 
+        char3{ trichar[2] - '0' };                              // и получить нужный int)
 
-    // Словари
-    string stringnums[] { " ", " один", " два", " три", " четыре", " пять", " шесть", " семь", " восемь", " девять" },
-        stringnumsteens[] { " ", " одиннадцать", " двенадцать", " тринадцать", " четырнадцать", " пятнадцать", " шестнадцать", " семнадцать", " восемнадцать", " девятнадцать" },
-        stringnumsdec[] { " ", " десять", " двадцать", " тридцать", " сорок", " пятьдесят", " шестьдесят", " семьдесят", " восемьдесят", " девяносто" },
-        stringnumshund[] { " ", " сто", " двести", " триста", " четыреста", " пятьсот", " шестьсот", " семьсот", " восемьсот", " девятьсот" };
+    std::string resultstring;
 
-    // Строка вывода
-    string resultstring;
-
-    // Читаем символы введённого массива
-    for (int i = sizeof(numbers) + 3; i >= 0; i--)
+    switch (cycles)                                             // Сколько циклов прошло?
     {
-        // Символ число?
-        if (isdigit(numbers[i]))
+    case 1:                                                     // Тысячи
+        if (char1 + char2 + char3 != 0)                         // Добавляются только если они есть
         {
-            if (trigger)
+            switch (char3)                                      // Правила русского языка
             {
-                if (numbers[i] == '1')
+            case 1:
+                if (char2 != 1)
                 {
-                    resultstring = " рубль";
+                    resultstring = g_strings[4][3];
+                    break;
                 }
-                else if (numbers[i] > '1' && numbers[i] < '5')
+            case 2:
+            case 3:
+            case 4:
+                if (char2 != 1)
                 {
-                    resultstring = " рубля";
+                    resultstring = g_strings[4][4];
+                    break;
                 }
-                else
+            default:
+                resultstring = g_strings[4][5];
+                break;
+            }
+        }
+        break;
+    case 2:                                                     // Миллионы                    
+        switch (char3)                                          // Тоже самое что и выше
+        {
+        case 1:
+            if (char2 != 1)
+            {
+                resultstring = g_strings[4][6];
+                break;
+            }
+        case 2:
+        case 3:
+        case 4:
+            if (char2 != 1)
+            {
+                resultstring = g_strings[4][7];
+                break;
+            }
+        default:
+            resultstring = g_strings[4][8];
+            break;
+        }
+        break;
+    default:
+        break;
+    }
+
+    if (char2 > 0)                                              // Проверка десятков
+    {
+        if (char2 == 1)                                         // Проверка на числа от 11 до 19
+        {
+            switch (char3)
+            {
+            case 0:                                             // Если нет единиц
+                resultstring = g_strings[2][char2] + resultstring;
+                break;
+            default:                                            // Если они есть
+                resultstring = g_strings[1][char3] + resultstring;
+                break;
+            }
+        }
+        else
+        {
+            switch (char3)                                          // Снова правила русского языка
+            {
+            case 1:
+                if (cycles == 1)
                 {
-                    resultstring = " рублей";
+                    resultstring = " одна" + resultstring;
+                    break;
+                }
+            case 2:
+                if (cycles == 1)
+                {
+                    resultstring = " две" + resultstring;
+                    break;
+                }
+            default:
+                resultstring = g_strings[0][char3] + resultstring;
+                break;
+            }
+
+            resultstring = g_strings[2][char2] + resultstring;
+        }
+    }
+    else                                                        // Если десятков нет, записываем только единицы
+    {
+        switch (char3)                                          // И снова правила русского языка
+        {
+        case 1:
+            if (cycles == 1)
+            {
+                resultstring = " одна" + resultstring;
+                break;
+            }
+        case 2:
+            if (cycles == 1)
+            {
+                resultstring = " две" + resultstring;
+                break;
+            }
+        default:
+            resultstring = g_strings[0][char3] + resultstring;
+            break;
+        }
+    }
+
+    if (char1 > 0)                                               // Проверка на сотни
+    {
+        resultstring = g_strings[3][char1] + resultstring;
+    }
+
+    return resultstring;
+}
+
+void numToTextConvert(char numbers[], bool trigger)                             // Основная функция конвертации
+{
+    char trichar[3] { 0,0,0 };                                                  // Разбивающий массив
+    int count{ 0 }, cycles{ 0 };                                                // Счётчики
+
+    std::string resultstring;                                                   // Строка вывода
+
+    for (int i = sizeof(numbers) + 3; i >= 0; i--)                              // Читаем символы введённого массива
+    {
+        if (isdigit(numbers[i]))                                                // Символ число?
+        {
+            if (trigger)                                                                                //⠉⠉⠉⣿⡿⠿⠛⠋⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⣻⣩⣉⠉⠉
+            {                                                                                           //⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢀⣀⣀⣀⣀⣀⣀⡀⠄⠄⠉⠉⠄⠄⠄
+                switch (numbers[i])                                                                     //⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣠⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣤⠄⠄⠄⠄
+                {                                                                                       //⠄⠄⠄⠄⠄⠄⠄⠄⠄⢤⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡀⠄⠄⠄
+                case 1:                                                                                 //⡄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠉⠄⠉⠉⠉⣋⠉⠉⠉⠉⠉⠉⠉⠉⠙⠛⢷⡀⠄⠄
+                    resultstring = g_strings[4][0];                                                     //⣿⡄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠠⣾⣿⣷⣄⣀⣀⣀⣠⣄⣢⣤⣤⣾⣿⡀⠄
+                    break;                                                                              //⣿⠃⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣹⣿⣿⡿⠿⣿⣿⣿⣿⣿⣿⣿⣿⢟⢁⣠
+                case 2:                                                                                 //⣿⣿⣄⣀⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠉⠉⣉⣉⣰⣿⣿⣿⣿⣷⣥⡀⠉⢁⡥⠈
+                case 3:                                                                                 //⣿⣿⣿⢹⣇⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠒⠛⠛⠋⠉⠉⠛⢻⣿⣿⣷⢀⡭⣤⠄
+                case 4:                                                                                 //⣿⣿⣿⡼⣿⠷⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢀⣀⣠⣿⣟⢷⢾⣊⠄⠄
+                    resultstring = g_strings[4][1];                                                     //⠉⠉⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠈⣈⣉⣭⣽⡿⠟⢉⢴⣿⡇⣺⣿⣷
+                    break;                                                                              //⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠁⠐⢊⣡⣴⣾⣥⣿⣿⣿
+                default:                                                                                //      Жёская нарезка кода
+                    resultstring = g_strings[4][2];
+                    break;
                 }
 
                 trigger = false;
             }
 
-            // Добавляем его в разбивной массив
-            trichar[2 - count] = numbers[i];
+            trichar[2 - count] = numbers[i];                                    // Добавляем его в разбивной массив
             count++;
 
-            // Если массив заполнен или числа закончились
-            if (count == 3 || i == 0)
+            if (count == 3 || i == 0)                                           // Если массив заполнен или числа закончились
             {
-                // Сколько циклов прошло?
-                switch (cycles)
-                {
-                // Тысячи
-                case 1:
-                    // Добавляются только если они есть
-                    if ((trichar[0] != '0' && trichar[2] != '0') || (trichar[1] != '0') || (trichar[0] != '0') || (trichar[2] != '0'))
-                    {
-                        // Правила русского языка
+                resultstring = writeToOutput(trichar, cycles) + resultstring;   // Функция анализа текущего разряда
 
-                        // Если первое число 1, а второе 0
-                        if ((trichar[2] == '1' && trichar[1] == '\0') || (trichar[2] == '1' && trichar[1] != '1'))
-                        {
-                            resultstring = " тысяча" + resultstring;
-                        }
-                        // Если первое число от 2 до 4
-                        else if (trichar[2] > '1' && trichar[2] < '5' && trichar[1] != '1')
-                        {
-                            resultstring = " тысячи" + resultstring;
-                        }
-                        else
-                        {
-                            resultstring = " тысяч" + resultstring;
-                        }
-                    }
-                    break;
-                // Миллионы
-                case 2:
-                    // Тоже самое что и выше
-                    if ((trichar[2] == '1' && trichar[1] == '\0') || (trichar[2] == '1' && trichar[1] != '1'))
-                    {
-                        resultstring = " миллион" + resultstring;
-                    }
-                    else if (trichar[2] > '1' && trichar[2] < '5' && trichar[1] != '1')
-                    {
-                        resultstring = " миллиона" + resultstring;
-                    }
-                    else
-                    {
-                        resultstring = " миллионов" + resultstring;
-                    } 
-                    break;
-                default:
-                    break;
-                }
-
-                // Временные переменные (символы хранятся как числа ASCII таблицы так что можно просто вычесть символ нуля(0) и получить нужный int)
-                int char1 = trichar[0] - '0', char2 = trichar[1] - '0', char3 = trichar[2] - '0';
-
-                /*———————————No switches? ———————————
-                    ⠀⣞⢽⢪⢣⢣⢣⢫⡺⡵⣝⡮⣗⢷⢽⢽⢽⣮⡷⡽⣜⣜⢮⢺⣜⢷⢽⢝⡽⣝
-                    ⠸⡸⠜⠕⠕⠁⢁⢇⢏⢽⢺⣪⡳⡝⣎⣏⢯⢞⡿⣟⣷⣳⢯⡷⣽⢽⢯⣳⣫⠇
-                    ⠀⠀⢀⢀⢄⢬⢪⡪⡎⣆⡈⠚⠜⠕⠇⠗⠝⢕⢯⢫⣞⣯⣿⣻⡽⣏⢗⣗⠏⠀
-                    ⠀⠪⡪⡪⣪⢪⢺⢸⢢⢓⢆⢤⢀⠀⠀⠀⠀⠈⢊⢞⡾⣿⡯⣏⢮⠷⠁⠀⠀
-                    ⠀⠀⠀⠈⠊⠆⡃⠕⢕⢇⢇⢇⢇⢇⢏⢎⢎⢆⢄⠀⢑⣽⣿⢝⠲⠉⠀⠀⠀⠀
-                    ⠀⠀⠀⠀⠀⡿⠂⠠⠀⡇⢇⠕⢈⣀⠀⠁⠡⠣⡣⡫⣂⣿⠯⢪⠰⠂⠀⠀⠀⠀
-                    ⠀⠀⠀⠀⡦⡙⡂⢀⢤⢣⠣⡈⣾⡃⠠⠄⠀⡄⢱⣌⣶⢏⢊⠂⠀⠀⠀⠀⠀⠀
-                    ⠀⠀⠀⠀⢝⡲⣜⡮⡏⢎⢌⢂⠙⠢⠐⢀⢘⢵⣽⣿⡿⠁⠁⠀⠀⠀⠀⠀⠀⠀
-                    ⠀⠀⠀⠀⠨⣺⡺⡕⡕⡱⡑⡆⡕⡅⡕⡜⡼⢽⡻⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-                    ⠀⠀⠀⠀⣼⣳⣫⣾⣵⣗⡵⡱⡡⢣⢑⢕⢜⢕⡝⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-                    ⠀⠀⠀⣴⣿⣾⣿⣿⣿⡿⡽⡑⢌⠪⡢⡣⣣⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-                    ⠀⠀⠀⡟⡾⣿⢿⢿⢵⣽⣾⣼⣘⢸⢸⣞⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-                    ⠀⠀⠀⠀⠁⠇⠡⠩⡫⢿⣝⡻⡮⣒⢽⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-                —————————————————————————————*/
-
-                // Проверка десятков
-                if (char2 != 0 && char2 > 0)
-                {
-                    // Проверка на числа от 11 до 19
-                    if (char2 == 1 && char3 > 0)
-                    {
-                        resultstring = stringnumsteens[char3] + resultstring;
-                    }
-                    else
-                    {
-                        // Снова правила русского языка
-                        if (trichar[2] == '1' && cycles == 1)
-                        {
-                            resultstring = " одна" + resultstring;
-                        }
-                        else if (trichar[2] == '2' && cycles == 1)
-                        {
-                            resultstring = " две" + resultstring;
-                        }
-                        else
-                        {
-                            resultstring = stringnums[char3] + resultstring;
-                        }
-
-                        resultstring = stringnumsdec[char2] + resultstring;
-                    }
-                }
-                // Если десятков нет, записываем только единицы
-                else
-                {
-                    // Снова правила русского языка
-                    if (trichar[2] == '1' && cycles == 1)
-                    {
-                        resultstring = " одна" + resultstring;
-                    }
-                    else if (trichar[2] == '2' && cycles == 1)
-                    {
-                        resultstring = " две" + resultstring;
-                    }
-                    else
-                    {
-                        resultstring = stringnums[char3] + resultstring;
-                    }
-                }
-
-                // Проверка на сотни
-                if (char1 != 0 && char1 > 0)
-                {
-                    resultstring = stringnumshund[char1] + resultstring;
-                }
-
-                // Увеличиваем счётчик, обнуляем массив
                 cycles++;
-                count = 0;
-                trichar[0] = 0; trichar[1] = 0; trichar[2] = 0;
+                count = 0;                                                      // Увеличиваем счётчик, обнуляем массив
+                trichar[0] = 0; 
+                trichar[1] = 0; 
+                trichar[2] = 0;
             }
         }
     }
 
-    // Выводим получившиюся строку
-    cout << resultstring;
+    std::cout << resultstring;                                                  // Выводим получившиюся строку
 
-    fill_n(numbers, 12, NULL);
+    std::fill_n(numbers, 12, NULL);                                             // Обнуляем ввод
 }
 
 int main()
@@ -188,7 +197,7 @@ int main()
 
     bool trigger;
 
-    cout << "Добавлять рубли к конечной строке на этот сеанс? (y/n)\n";
+    std::cout << "Добавлять рубли к конечной строке на этот сеанс? (y/n)\n";
 
 tryagain:
 
@@ -206,45 +215,21 @@ tryagain:
         goto tryagain;
         break;
     }
-    //———————————Вечный цикл? ———————————
-    //    ⠀⣞⢽⢪⢣⢣⢣⢫⡺⡵⣝⡮⣗⢷⢽⢽⢽⣮⡷⡽⣜⣜⢮⢺⣜⢷⢽⢝⡽⣝
-    //    ⠸⡸⠜⠕⠕⠁⢁⢇⢏⢽⢺⣪⡳⡝⣎⣏⢯⢞⡿⣟⣷⣳⢯⡷⣽⢽⢯⣳⣫⠇
-    //    ⠀⠀⢀⢀⢄⢬⢪⡪⡎⣆⡈⠚⠜⠕⠇⠗⠝⢕⢯⢫⣞⣯⣿⣻⡽⣏⢗⣗⠏⠀
-    //    ⠀⠪⡪⡪⣪⢪⢺⢸⢢⢓⢆⢤⢀⠀⠀⠀⠀⠈⢊⢞⡾⣿⡯⣏⢮⠷⠁⠀⠀
-    //    ⠀⠀⠀⠈⠊⠆⡃⠕⢕⢇⢇⢇⢇⢇⢏⢎⢎⢆⢄⠀⢑⣽⣿⢝⠲⠉⠀⠀⠀⠀
-    //    ⠀⠀⠀⠀⠀⡿⠂⠠⠀⡇⢇⠕⢈⣀⠀⠁⠡⠣⡣⡫⣂⣿⠯⢪⠰⠂⠀⠀⠀⠀
-    //    ⠀⠀⠀⠀⡦⡙⡂⢀⢤⢣⠣⡈⣾⡃⠠⠄⠀⡄⢱⣌⣶⢏⢊⠂⠀⠀⠀⠀⠀⠀
-    //    ⠀⠀⠀⠀⢝⡲⣜⡮⡏⢎⢌⢂⠙⠢⠐⢀⢘⢵⣽⣿⡿⠁⠁⠀⠀⠀⠀⠀⠀⠀
-    //    ⠀⠀⠀⠀⠨⣺⡺⡕⡕⡱⡑⡆⡕⡅⡕⡜⡼⢽⡻⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-    //    ⠀⠀⠀⠀⣼⣳⣫⣾⣵⣗⡵⡱⡡⢣⢑⢕⢜⢕⡝⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-    //    ⠀⠀⠀⣴⣿⣾⣿⣿⣿⡿⡽⡑⢌⠪⡢⡣⣣⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-    //    ⠀⠀⠀⡟⡾⣿⢿⢿⢵⣽⣾⣼⣘⢸⢸⣞⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-    //    ⠀⠀⠀⠀⠁⠇⠡⠩⡫⢿⣝⡻⡮⣒⢽⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-    //    —————————————————————————————
-    while (true)
-    {
-        // Массив ввода
-        char numbers[12];
 
-        cout << "Введите конвертируемое число: \n";
-
-        // Чтение строки из 12 символов (на случай разделителей)
-        cin.getline(numbers, 12);
-
-        cout << "Результат:\n";
-
-        // Вызов функции
-        numToTextConvert(numbers, trigger);
-
-        cout << "\nДля повторного набора нажмите <r> , для выхода нажмите любую другую клавишу.";
-
-        // Выход из цикла
-        if (_getch() != 'r')
-        {
-            exit(0);
-        }
-
-        // Чистка экрана
-        system("cls");
+    while (true)                                                                                                    //———————————Вечный цикл? ———————————
+    {                                                                                                               //    ⠀⣞⢽⢪⢣⢣⢣⢫⡺⡵⣝⡮⣗⢷⢽⢽⢽⣮⡷⡽⣜⣜⢮⢺⣜⢷⢽⢝⡽⣝
+        char numbers[12];                               // Массив ввода                                             //    ⠸⡸⠜⠕⠕⠁⢁⢇⢏⢽⢺⣪⡳⡝⣎⣏⢯⢞⡿⣟⣷⣳⢯⡷⣽⢽⢯⣳⣫⠇
+                                                                                                                    //    ⠀⠀⢀⢀⢄⢬⢪⡪⡎⣆⡈⠚⠜⠕⠇⠗⠝⢕⢯⢫⣞⣯⣿⣻⡽⣏⢗⣗⠏⠀
+        std::cout << "Введите конвертируемое число: \n";                                                            //    ⠀⠪⡪⡪⣪⢪⢺⢸⢢⢓⢆⢤⢀⠀⠀⠀⠀⠈⢊⢞⡾⣿⡯⣏⢮⠷⠁
+        std::cin.getline(numbers, 12);                  // Чтение строки из 12 символов (на случай разделителей)    //    ⠀⠀⠀⠈⠊⠆⡃⠕⢕⢇⢇⢇⢇⢇⢏⢎⢎⢆⢄⠀⢑⣽⣿⢝⠲⠉⠀
+        std::cout << "Результат:\n";                                                                                //    ⠀⠀⠀⠀⠀⡿⠂⠠⠀⡇⢇⠕⢈⣀⠀⠁⠡⠣⡣⡫⣂⣿⠯⢪⠰⠂
+                                                                                                                    //    ⠀⠀⠀⠀⡦⡙⡂⢀⢤⢣⠣⡈⣾⡃⠠⠄⠀⡄⢱⣌⣶⢏⢊⠂⠀
+        numToTextConvert(numbers, trigger);             // Вызов функции                                            //    ⠀⠀⠀⠀⢝⡲⣜⡮⡏⢎⢌⢂⠙⠢⠐⢀⢘⢵⣽⣿⡿⠁⠁⠀⠀
+                                                                                                                    //    ⠀⠀⠀⠀⠨⣺⡺⡕⡕⡱⡑⡆⡕⡅⡕⡜⡼⢽⡻⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+        std::cout << "\nДля повторного набора нажмите <r> , для выхода нажмите любую другую клавишу.";              //    ⠀⠀⠀⠀⣼⣳⣫⣾⣵⣗⡵⡱⡡⢣⢑⢕⢜⢕⡝⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+        if (_getch() != 'r')                            // Выход из цикла                                           //    ⠀⠀⠀⣴⣿⣾⣿⣿⣿⡿⡽⡑⢌⠪⡢⡣⣣⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+            exit(0);                                                                                                //    ⠀⠀⠀⡟⡾⣿⢿⢿⢵⣽⣾⣼⣘⢸⢸⣞⡟⠀
+                                                                                                                    //    ⠀⠀⠀⠀⠁⠇⠡⠩⡫⢿⣝⡻⡮⣒⢽⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+        system("cls");                                  // Чистка экрана                                            //  —————————————————————————————
     }
 }
